ee5b6feb26c92cc41c524d56007e2e5c1457fbf7
Lev Alhazov <alhazovlev@gmail.com>
2020-11-29 16:26:25 +0200

    Added functional registration page + account database

--- hackathon/G_neighbour/views.py
+++ hackathon/G_neighbour/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+def signupuser(request):
+    if request.method=='GET':
+        return render(request, 'signupuser.html' , {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('index')
+
+
+            except IntegrityError:
+                return render(request, 'signupuser.html', {'form': UserCreationForm(), 'error': 'The user already exists'})
+        else:
+            return render(request, 'signupuser.html' , {'form':UserCreationForm(), 'error':'Passwords did not match'})
+            # Passwords don't match.
+
+
 
--- hackathon/hackathon/urls.py
+++ hackathon/hackathon/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from G_neighbour import views
+
 
 urlpatterns = [
     path('',include('G_neighbour.urls')),
     path('admin/', admin.site.urls),
+    path('signup/', views.signupuser, name='signupuser'),
 ]
